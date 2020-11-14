c30c6085dfd8803d4ea10b7560bde423b1e6b231
leihao <leihao@xiaoduotech.com>
2020-11-14 15:03:01 +0800

    first commit

---
+++ webpack.dev.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  /*入口*/
+  mode: 'development',
+  entry: path.join(__dirname, 'src/index.js'),
+  /*输出到dist文件夹，输出文件名字为bundle.js*/
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'bundle.js'
+  }
+};
